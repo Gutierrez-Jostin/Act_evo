3a60d958cb5e944cc9fc64336bc90d26dfd22d0d
Gutierrez-Jostin <elenamarquez2000casilla@gmail.com>
2026-04-21 19:31:17 -0700

    Guarda mis cambios

--- dj_my_books/books/views.py
+++ dj_my_books/books/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .models import Books, Category
 
 # Create your views here.
+def home(request):
+    books = Books.objects.all()
+    context = {
+        'books': books
+    }
+    return render(request, 'books/home.html', context)
+
+def detail(request, id):
+    book = get_object_or_404(Books, id=id)
+
+    context = {
+        'book': book,
+    }
+
+    return render(request, 'blog/detail.html', context)
